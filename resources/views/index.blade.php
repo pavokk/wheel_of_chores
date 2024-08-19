a68229867ec286737cd858ef394eaf231dcfadc5
Paul Volden <paul@latitude-paul.home>
2024-08-19 23:19:17 +0200

    Changed the index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wheel of chores</title>
+    </head>
+    <body style="background-color:#070D0D">
+        <h1 style="text-align:center;color:#fbfbfb;">Heia Kamilla</h1>
+    </body>
+</html>
