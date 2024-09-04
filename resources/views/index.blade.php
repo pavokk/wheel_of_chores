d5017f0a4e0da26ef3b712edd87171383c4ad096
Paul Volden <pavokk@gmail.com>
2024-09-04 13:47:05 +0200

    Added the wheel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,51 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Wheel of chores</title>
+
+        <style>
+
+            main {
+                max-width: 800px;
+                width: 100%;
+                margin: 0 auto;
+            }
+
+            .wheel-of-fortune {
+                clip-path: inset(0 0 0 0 round 50%);
+            }
+
+            :where(.wheel-of-fortune) {
+                --_items: {{ count($db['chores']) }};
+                all: unset;
+                aspect-ratio: 1 / 1;
+                background: crimson;
+                container-type: inline-size;
+                direction: ltr;
+                display: grid;
+                place-content: center start;
+            }
+
+            li {
+                align-content: center;
+                display: grid;
+                font-size: 3cqi;
+                grid-area: 1 / -1;
+                list-style: none;
+                padding-left: 1ch;
+                transform-origin: center right;
+                width: 50cqi;
+                rotate: calc(360deg / var(--_items) * calc(var(--_idx) - 1));
+                background : hsl(calc(360deg / var(--_items) * calc(var(--_idx))), 100%, 75%);
+                height: calc((2.5 * pi * 50cqi) / var(--_items));
+                clip-path: polygon(0% -2%, 100% 50%, 0% 102%);
+            }
+
+            li p {
+                margin-left: 15px;
+            }
+
+        </style>
+
     </head>
     <body style="background-color:#070D0D">
 
@@ -16,11 +61,11 @@
 
         <main>
 
-            <ul>
+            <ul class="wheel-of-fortune">
 
                 @foreach ($db['chores'] as $chore)
 
-                <li>{{ $chore->name  }}</li>
+                <li style="--_idx: {{ $loop->index }}"><p>{{ $chore->name  }}</p></li>
 
                 @endforeach
 
